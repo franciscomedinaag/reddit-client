d78277f31d4f4cc4a3cdfd4191b64c38377171e5
unknown <a16300232@live.ceti.mx>
2021-06-11 23:49:33 -0500

    format reddit data

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -13,16 +13,33 @@ export class SidebarComponent implements OnInit {
   public offset:number=0;
   public postQuantity:number=10; //show 10 posts
   public postLimit:number=50; //show 10 posts
-
+  public queryAfter:number=50; //show 10 posts
+  public postDataArray:Array<any>=[]
 
   ngOnInit(): void {
-    this.getPosts(this.offset, this.postQuantity)
+    this.getInitialPosts(this.offset, this.postQuantity)
   }
 
-  getPosts(offset:number, postQuantity:number): void{
+  getInitialPosts(offset:number, postQuantity:number): void{
     this.api.get('https://www.reddit.com/r/all/top.json?count='+offset+'&limit='+postQuantity)
     .subscribe(async (posts:any)=>{
-      console.log("posts: ", posts)
+      this.queryAfter=posts.data.after
+      posts.data.children.forEach((postRawData:any) => {
+        let postData = {
+          id:postRawData.data.id,
+          title:postRawData.data.title,
+          author:postRawData.data.author,
+          created:postRawData.data.created_utc,
+          thumb:postRawData.data.thumbnail,
+          num_comments:postRawData.data.num_comments,
+          seen:false,
+          media:postRawData.data.media ? postRawData.data.media.reddit_video.fallback_url : postRawData.data.url,
+          video:Boolean(postRawData.data.media)
+        }
+        this.postDataArray.push(postData)
+      });
+
+      console.log(this.postDataArray)
     })
   }
 
