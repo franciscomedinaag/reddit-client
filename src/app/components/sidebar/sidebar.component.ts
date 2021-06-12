ca682bcd52e6145a3d4312b0f9c093b8287d18ab
unknown <a16300232@live.ceti.mx>
2021-06-12 00:11:31 -0500

    added time ago label

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -29,7 +29,7 @@ export class SidebarComponent implements OnInit {
           id:postRawData.data.id,
           title:postRawData.data.title,
           author:postRawData.data.author,
-          created:postRawData.data.created_utc,
+          created:this.timeSince(new Date(postRawData.data.created * 1000)) + " ago",
           thumb:postRawData.data.thumbnail,
           num_comments:postRawData.data.num_comments,
           seen:false,
@@ -38,9 +38,34 @@ export class SidebarComponent implements OnInit {
         }
         this.postDataArray.push(postData)
       });
-
       console.log(this.postDataArray)
     })
+  }
+
+   timeSince(date:any) {
+    var seconds = Math.floor((+new Date() - date) / 1000); 
+    var interval = seconds / 31536000;
+  
+    if (interval > 1) {
+      return Math.floor(interval) + " years";
+    }
+    interval = seconds / 2592000;
+    if (interval > 1) {
+      return Math.floor(interval) + " months";
+    }
+    interval = seconds / 86400;
+    if (interval > 1) {
+      return Math.floor(interval) + " days";
+    }
+    interval = seconds / 3600;
+    if (interval > 1) {
+      return Math.floor(interval) + " hours";
+    }
+    interval = seconds / 60;
+    if (interval > 1) {
+      return Math.floor(interval) + " minutes";
+    }
+    return Math.floor(seconds) + " seconds";
   }
 
 }
